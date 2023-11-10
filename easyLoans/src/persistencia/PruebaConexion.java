39f0caec8d5ca4633fe5e86382332aebde503709
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-11-10 01:24:01 -0500

    agregando detalles a la prueba

--- easyLoans/src/persistencia/PruebaConexion.java
+++ easyLoans/src/persistencia/PruebaConexion.java
@@ -5,13 +5,13 @@ import persistencia.ConexionBD.ConexionDB;
 
 public class PruebaConexion {
     public static void main(String[] args) {
-        ConexionDB conexion = new ConexionMysql(); // Instancia de la conexión
+        ConexionDB conexion = new ConexionMysql(); 
 
-        Connection connection = conexion.obtenerConexion(); // Intentar obtener la conexión
+        Connection connection = conexion.obtenerConexion(); 
 
         if (connection != null) {
             System.out.println("Conexion exitosa a la base de datos.");
-            conexion.cerrarConexion(); // Cerrar la conexión luego de la prueba
+            conexion.cerrarConexion(); 
         } else {
             System.out.println("Error al conectar a la base de datos.");
         }
