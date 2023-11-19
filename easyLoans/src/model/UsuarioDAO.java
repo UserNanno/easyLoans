35307c4d1a51ab54c3dd246a2cbc2546802b0ad1
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-11-19 15:06:40 -0500

    Revert "Adding HashSet"
    This reverts commit 3d515ea29ff0254c05023d78c6b53b61517cc612.

--- easyLoans/src/model/UsuarioDAO.java
+++ easyLoans/src/model/UsuarioDAO.java
@@ -1,64 +1,53 @@
 package model;
 
+import persistencia.ConexionBD;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import persistencia.ConexionBD;
-import util.DniManager;
-
 public class UsuarioDAO {
 
-    private final ConexionBD conexion;
-    private final DniManager dniManager;
+    private ConexionBD conexion;
 
     public UsuarioDAO() {
         this.conexion = ConexionBD.obtenerInstancia();
-        this.dniManager = new DniManager();
-        cargarUsuariosRegistrados();
-    }
-
-    private void cargarUsuariosRegistrados() {
-        try (Connection connection = conexion.obtenerConexion(); PreparedStatement preparedStatement = connection.prepareStatement("SELECT dni_usuario FROM usuarios"); ResultSet resultSet = preparedStatement.executeQuery()) {
-
-            while (resultSet.next()) {
-                dniManager.agregarDni(resultSet.getString("dni_usuario"));
-            }
-        } catch (SQLException e) {
-            System.out.println("Error al cargar usuarios registrados: " + e.getMessage());
-        }
     }
 
     public boolean verificarUsuario(String dni) {
-        return dniManager.contieneDni(dni);
+    try {
+        if (conexion != null) {
+            Connection connection = conexion.obtenerConexion();
+            if (connection != null) {
+                String query = "SELECT * FROM usuarios WHERE dni_usuario = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, dni);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                return resultSet.next(); // Si existe un resultado, el usuario existe
+            }
+        }
+    } catch (SQLException e) {
+        System.out.println("Error al verificar usuario: " + e.getMessage());
     }
+    return false; // Si hay errores o no se encuentra el usuario
+}
 
     public boolean registrarNuevoUsuario(String dni, String apellidos, String nombres) {
-        try (Connection connection = conexion.obtenerConexion()) {
-            connection.setAutoCommit(false);
+        String query = "INSERT INTO usuarios (dni_usuario, apellidos, nombres) VALUES (?, ?, ?)";
+        try {
+            if (conexion != null) {
+                PreparedStatement pstmt = conexion.obtenerConexion().prepareStatement(query);
+                pstmt.setString(1, dni);
+                pstmt.setString(2, apellidos);
+                pstmt.setString(3, nombres);
 
-            if (!dniManager.contieneDni(dni)) {
-                String insertQuery = "INSERT INTO usuarios (dni_usuario, apellidos, nombres) VALUES (?, ?, ?)";
-                try (PreparedStatement pstmt = connection.prepareStatement(insertQuery)) {
-                    pstmt.setString(1, dni);
-                    pstmt.setString(2, apellidos);
-                    pstmt.setString(3, nombres);
-
-                    int filasAfectadas = pstmt.executeUpdate();
-                    if (filasAfectadas > 0) {
-                        dniManager.agregarDni(dni);
-                        connection.commit();
-                        return true;
-                    }
-                }
+                int filasAfectadas = pstmt.executeUpdate();
+                return filasAfectadas > 0; // Retorna true si se insertaron filas en la tabla
             }
-
-            connection.rollback(); // Revierte la transacción si no se cumple la condición
         } catch (SQLException e) {
             System.out.println("Error al registrar usuario: " + e.getMessage());
         }
         return false;
     }
-
 }
